409ccaf0b3706d9f7b7427d847dab52a82655a55
bkkom <seoy1108@gmail.com>
2023-10-04 15:15:51 +0900

    ✨ Feat: Header 이동하는 Icon

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,38 @@
 import React from 'react';
+import { Link, useMatch } from 'react-router-dom';
 
 function Header() {
+  const homeMatch = useMatch('/');
+  const comingMatch = useMatch('/coming-soon');
+  const nowMatch = useMatch('/now-playing');
   return (
-    <>
-      <span>Header</span>
-    </>
+    <ul>
+      <li>
+        <Link to="/">popular {homeMatch && <Circle />}</Link>
+      </li>
+      <li>
+        <Link to="/coming-soon">coming soon {comingMatch && <Circle />}</Link>
+      </li>
+      <li>
+        <Link to="/now-playing">now playing {nowMatch && <Circle />}</Link>
+      </li>
+    </ul>
   );
 }
+
+const Circle = () => {
+  return (
+    <span
+      style={{
+        width: '5px',
+        height: '5px',
+        borderRadius: '5px',
+        backgroundColor: 'red',
+      }}
+    >
+      ⬅️
+    </span>
+  );
+};
 
 export default Header;
