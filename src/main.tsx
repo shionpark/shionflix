5e4e521edd20a21572f2811e5bdab426f2b4d56c
bkkom <seoy1108@gmail.com>
2023-10-04 15:12:12 +0900

    ➕ Add: react-query

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,15 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { RouterProvider } from 'react-router-dom';
-import { router } from './routers';
+import { router } from './router';
+
+const client = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <QueryClientProvider client={client}>
+      <RouterProvider router={router} />
+    </QueryClientProvider>
   </React.StrictMode>
 );
