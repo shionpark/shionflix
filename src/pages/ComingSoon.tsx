818baa3887d8e8cc6fdf3f4c497e808765a049a8
bkkom <seoy1108@gmail.com>
2023-10-04 15:30:50 +0900

    💩 Chore: 비동기 데이터 가져오기

--- src/pages/ComingSoon.tsx
+++ src/pages/ComingSoon.tsx
@@ -1,9 +1,31 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { IMovie, getComingSoon, makeImagePath } from '@/utils';
 
 function ComingSoon() {
+  const [popularMovies, setPopularMovies] = useState<IMovie[]>([]);
+
+  useEffect(() => {
+    // 비동기 데이터 가져오기
+    getComingSoon()
+      .then((data) => {
+        // 데이터를 받아와서 상태 변수에 저장
+        setPopularMovies(data.results);
+        console.log(data.results);
+      })
+      .catch((error) => {
+        console.error('Error fetching data:', error);
+      });
+  }, []);
+
   return (
     <>
       <h1>Coming Soon</h1>
+      {popularMovies.map((movie) => (
+        <div key={movie.id}>
+          <img style={{ width: '160px', height: '200px' }} src={makeImagePath(movie.poster_path)} />
+          <h3 key={movie.id}>{movie.original_title}</h3>
+        </div>
+      ))}
     </>
   );
 }
