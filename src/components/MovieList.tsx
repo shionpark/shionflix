cb588d045d64982ac6c6b5381d61b964b7e0e71a
bkkom <seoy1108@gmail.com>
2023-10-07 11:11:18 +0900

    💩Feat: Modal Detail 페이지

--- src/components/MovieList.tsx
+++ src/components/MovieList.tsx
@@ -21,21 +21,77 @@ export function MovieList({ dataKey, fetchData }: IListProps) {
 
   const params = useParams();
   const isOverlayVisible = params.movieId !== undefined;
+  const clickedMovie =
+    params.movieId && data?.results.find((moive: IMovie) => String(moive.id) === params.movieId);
 
   return (
     <>
       {isOverlayVisible ? (
-        <div
-          style={{
-            position: 'fixed',
-            top: '0',
-            width: '100%',
-            height: '100%',
-            backgroundColor: 'rgba(0, 0, 0, 0.5)',
-            opacity: '1',
-          }}
-          onClick={() => navigate(-1)}
-        ></div>
+        <>
+          <div
+            style={{
+              position: 'fixed',
+              top: '0',
+              width: '100%',
+              height: '100%',
+              backgroundColor: 'rgba(0, 0, 0, 0.5)',
+              opacity: '1',
+            }}
+            onClick={() => navigate(-1)}
+          >
+            overlay
+            <div
+              style={{
+                position: 'absolute',
+                width: '40vw',
+                height: '80vh',
+                left: '0',
+                right: '0',
+                margin: '0 auto',
+                borderRadius: '15px',
+                overflow: 'hidden',
+                backgroundColor: 'black',
+              }}
+            >
+              {clickedMovie && (
+                <>
+                  <div
+                    style={{
+                      width: '100%',
+                      backgroundSize: 'cover',
+                      backgroundPosition: 'center center',
+                      height: '400px',
+                      backgroundImage: `linear-gradient(to top, black, transparent), url(${makeImagePath(
+                        clickedMovie.backdrop_path
+                      )})`,
+                    }}
+                  ></div>
+                  <h3
+                    style={{
+                      color: 'white',
+                      padding: '20px',
+                      fontSize: '46px',
+                      position: 'relative',
+                      top: '-80px',
+                    }}
+                  >
+                    {clickedMovie.title}
+                  </h3>
+                  <p
+                    style={{
+                      padding: '20px',
+                      position: 'relative',
+                      top: '-80px',
+                      color: 'white',
+                    }}
+                  >
+                    {clickedMovie.overview}
+                  </p>
+                </>
+              )}
+            </div>
+          </div>
+        </>
       ) : null}
       {isLoading && <h2>Loading...</h2>}
       {isError && <h2>{isError}</h2>}
