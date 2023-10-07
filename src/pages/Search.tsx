a345fb1b4f087cf0bc93226d5feebbb92092bb98
bkkom <seoy1108@gmail.com>
2023-10-07 22:17:21 +0900

    💩 Chore : Search Form 검색 기능

--- src/pages/Search.tsx
+++ src/pages/Search.tsx
@@ -1,12 +1,42 @@
 import React from 'react';
-import { useLocation } from 'react-router-dom';
+import { useLocation, useNavigate } from 'react-router-dom';
+import { useQuery } from '@tanstack/react-query';
+import { IMovieResponse, getPopular, makeImagePath } from '@/utils';
 
 const Search = () => {
   const {
     state: { keyword },
   } = useLocation();
-  console.log(keyword);
-  return <h1>Search</h1>;
+
+  console.log('search : ', keyword);
+
+  const { data, isLoading, isError } = useQuery<IMovieResponse>(['popular'], getPopular);
+  console.log(data);
+
+  const navigate = useNavigate();
+  const location = useLocation();
+  const currentLocation = location.pathname;
+  const getDetailPath = (movieId: number) => {
+    return currentLocation === '/' ? `/movie/${movieId}` : `${currentLocation}/movie/${movieId}`;
+  };
+
+  return (
+    <div>
+      <h1 style={{ color: 'red' }}>검색 결과</h1>
+      {data?.results
+        .filter((movie) => (keyword ? movie.title.toLowerCase().includes(keyword) : true))
+        .map((movie, index) => (
+          <>
+            <img
+              style={{ width: '160px', height: '200px' }}
+              src={makeImagePath(movie.poster_path)}
+              onClick={() => navigate(getDetailPath(movie.id))}
+            />
+            <h1 key={index}>{movie.title}</h1>
+          </>
+        ))}
+    </div>
+  );
 };
 
 export default Search;
