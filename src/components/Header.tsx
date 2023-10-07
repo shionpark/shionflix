532e05f724daf4159556a52e8467b85ee99bbd0b
bkkom <seoy1108@gmail.com>
2023-10-08 08:45:10 +0900

    💄 Design: Header 로고 및 애니메이션

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import { Link, useMatch, useNavigate } from 'react-router-dom';
 import { useForm } from 'react-hook-form';
-
+import styled from 'styled-components';
+import { motion, Variants } from 'framer-motion';
 interface IForm {
   keyword: string;
 }
@@ -24,40 +25,93 @@ function Header() {
   };
 
   return (
-    <>
-      <ul>
-        <li>
-          <Link to="/">popular {homeMatch && <Circle />}</Link>
-        </li>
-        <li>
-          <Link to="/coming-soon">coming soon {comingMatch && <Circle />}</Link>
-        </li>
-        <li>
-          <Link to="/now-playing">now playing {nowMatch && <Circle />}</Link>
-        </li>
-      </ul>
-      <form onSubmit={handleSubmit(onValid)}>
-        <input {...register('keyword')} placeholder="search" />
-        <button>Click</button>
-      </form>
-      <span>{errors?.keyword?.message}</span>
-    </>
+    <Nav>
+      <Col>
+        <Logo
+          variants={logoVariants}
+          as={motion.span}
+          initial="initial"
+          animate="animate"
+          transition={{ type: 'spring' }}
+        >
+          <Link to="/">Shionflix </Link>
+        </Logo>
+        <Items>
+          <Item>
+            <Link to="/">popular {homeMatch && <Circle layoutId="circle" />}</Link>
+          </Item>
+          <Item>
+            <Link to="/coming-soon">coming soon {comingMatch && <Circle layoutId="circle" />}</Link>
+          </Item>
+          <Item>
+            <Link to="/now-playing">now playing {nowMatch && <Circle layoutId="circle" />}</Link>
+          </Item>
+        </Items>
+      </Col>
+      <Col>
+        <form onSubmit={handleSubmit(onValid)}>
+          <input {...register('keyword')} placeholder="search" />
+          <button>Click</button>
+        </form>
+        <span>{errors?.keyword?.message}</span>
+      </Col>
+    </Nav>
   );
 }
 
-const Circle = () => {
-  return (
-    <span
-      style={{
-        width: '5px',
-        height: '5px',
-        borderRadius: '5px',
-        backgroundColor: 'red',
-      }}
-    >
-      ⬅️
-    </span>
-  );
+const Nav = styled.nav`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  position: fixed;
+  width: 100%;
+  top: 0;
+  background-color: black;
+  font-size: 14px;
+  padding: 20px 60px;
+  color: white;
+`;
+
+const Col = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+const logoVariants: Variants = {
+  initial: { scale: 0.8, opacity: 0.4 },
+  animate: { scale: 1.0, opacity: 1 },
 };
+
+const Logo = styled(motion.span)`
+  margin-right: 50px;
+  color: red;
+  font-weight: bold;
+`;
+
+const Items = styled.ul`
+  display: flex;
+  align-items: center;
+`;
+
+const Item = styled.li`
+  margin-right: 20px;
+  transition: color 0.3s ease-in-out;
+  position: relative;
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+`;
+
+const Circle = styled(motion.span)`
+  position: absolute;
+  width: 5px;
+  height: 5px;
+  border-radius: 2.5px;
+  bottom: -5px;
+  left: 0;
+  right: 0;
+  margin: 0 auto;
+  background-color: red;
+`;
 
 export default Header;
