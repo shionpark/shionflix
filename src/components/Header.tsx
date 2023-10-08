1be188d330be435bfb0bfe0eae12f30d21542f8f
bkkom <seoy1108@gmail.com>
2023-10-08 09:07:08 +0900

    💄 Design: Search Form 애니메이션

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,16 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link, useMatch, useNavigate } from 'react-router-dom';
 import { useForm } from 'react-hook-form';
 import styled from 'styled-components';
-import { motion, Variants } from 'framer-motion';
+import { motion, Variants, useAnimation } from 'framer-motion';
 interface IForm {
   keyword: string;
 }
 
 function Header() {
+  const [searchOpen, setSearchOpen] = useState(false);
   const homeMatch = useMatch('/');
   const comingMatch = useMatch('/coming-soon');
   const nowMatch = useMatch('/now-playing');
+  const inputAnimation = useAnimation();
+
+  const toggleSearch = () => {
+    if (searchOpen) {
+      inputAnimation.start({
+        scaleX: 0,
+      });
+    } else {
+      inputAnimation.start({ scaleX: 1 });
+    }
+    setSearchOpen((prev) => !prev);
+  };
 
   const {
     register,
@@ -49,11 +62,29 @@ function Header() {
         </Items>
       </Col>
       <Col>
-        <form onSubmit={handleSubmit(onValid)}>
-          <input {...register('keyword')} placeholder="search" />
-          <button>Click</button>
-        </form>
-        <span>{errors?.keyword?.message}</span>
+        <Search onSubmit={handleSubmit(onValid)}>
+          <motion.svg
+            onClick={toggleSearch}
+            animate={{ x: searchOpen ? -185 : 0 }}
+            transition={{ type: 'linear' }}
+            fill="currentColor"
+            viewBox="0 0 20 20"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              fillRule="evenodd"
+              d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"
+              clipRule="evenodd"
+            ></path>
+          </motion.svg>
+          <Input
+            {...register('keyword', { required: true, minLength: 2 })}
+            animate={inputAnimation}
+            initial={{ scaleX: 0 }}
+            transition={{ type: 'linear' }}
+            placeholder="Search title"
+          />
+        </Search>
       </Col>
     </Nav>
   );
@@ -112,6 +143,29 @@ const Circle = styled(motion.span)`
   right: 0;
   margin: 0 auto;
   background-color: red;
+`;
+
+const Search = styled.form`
+  color: white;
+  display: flex;
+  align-items: center;
+  position: relative;
+  svg {
+    height: 25px;
+  }
+`;
+
+const Input = styled(motion.input)`
+  transform-origin: right center;
+  position: absolute;
+  right: 0px;
+  padding: 5px 10px;
+  padding-left: 40px;
+  z-index: -1;
+  color: white;
+  font-size: 16px;
+  background-color: transparent;
+  border: 0.8px solid whitesmoke;
 `;
 
 export default Header;
